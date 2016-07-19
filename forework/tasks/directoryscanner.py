f26134bd84481b3a23c84d1ca52eeb55626b28e4
insomniac <insomniac@slackware.it>
2016-07-19 17:03:44 +0100

    DirectoryScanner now using utils.get_file_type

--- forework/tasks/directoryscanner.py
+++ forework/tasks/directoryscanner.py
@@ -3,8 +3,6 @@ import os
 from ..basetask import BaseTask, find_tasks_by_filetype
 from .. import utils
 
-import magic
-
 
 logger = utils.get_logger(__name__)
 
@@ -18,17 +16,12 @@ class DirectoryScanner(BaseTask):
 
     def run(self):
         logger.info('Scanning directory point {mp}'.format(mp=self._path))
-        mage = magic.Magic()
-        # listdir can be put in trouble with directories with a large number of
-        # files.
-        # TODO use a proper streaming scheduler and yield the files while they
-        # are found
+        # FIXME listdir can be put in trouble with directories with a large
+        # number of files. Use a proper streaming scheduler and yield the files
+        # while they are found
         for dirent in os.listdir(self._path):
             path = os.path.join(self._path, dirent)
-            if os.path.isdir(path):
-                filetype = 'directory'
-            else:
-                filetype = mage.from_file(path)
+            filetype = utils.get_file_type(path)
             tasknames = find_tasks_by_filetype(filetype)
             if len(tasknames) < 1:
                 msg = 'Cannot find a task for {fn}'.format(fn=path)
